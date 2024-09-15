7c1a58c2461ccdc8ceb2dbc1f88554b41e730365
André Uchôa <uchoa@pobox.com>
2024-09-14 22:25:52 -0300

    modify wezterm to look better in macOS

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,14 +6,14 @@ config = {
 	enable_tab_bar = false,
 	window_decorations = 'RESIZE',
 	default_cursor_style = 'BlinkingBar',
-	window_background_opacity = 0.9,
+	window_background_opacity = 0.95,
 	window_close_confirmation = 'NeverPrompt',
 	font = wezterm.font('InconsolataGo Nerd Font Mono'),
-	font_size = 14,
+	font_size = 18,
 
 	color_scheme = 'Vs Code Dark+ (Gogh)',
 
-	default_prog = { 'tmux' },
+	--default_prog = { 'tmux' },
 }
 
 return config
