9630372773ab7ad32d1ea74777c64c8fb8307807
André Uchôa <uchoa@pobox.com>
2024-09-15 00:49:53 -0300

    set wezterm transparency and blur; start tmux automatically

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,14 +6,15 @@ config = {
 	enable_tab_bar = false,
 	window_decorations = 'RESIZE',
 	default_cursor_style = 'BlinkingBar',
-	window_background_opacity = 0.95,
+	window_background_opacity = 0.3,
+	macos_window_background_blur = 100,
 	window_close_confirmation = 'NeverPrompt',
 	font = wezterm.font('InconsolataGo Nerd Font Mono'),
 	font_size = 18,
 
 	color_scheme = 'Vs Code Dark+ (Gogh)',
 
-	--default_prog = { 'tmux' },
+	default_prog = { '/usr/local/bin/tmux' },
 }
 
 return config
