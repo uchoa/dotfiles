0a977a55c51dbb3317b789fb44880ea832ffea50
André Uchôa <uchoa@pobox.com>
2026-01-12 02:32:37 -0300

    fix status line colors after No Clown Fiesta theme

--- nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,21 +1,34 @@
 local opts = {
-  theme = "dark",
+	theme = "dark",
 	transparent = true,
-  styles = {
-    type = { bold = true },
-    lsp = { underline = false },
-    match_paren = { underline = true },
-  },
+	styles = {
+		comments = { italic = true },
+		type = { bold = true },
+		lsp = { underline = false },
+		match_paren = { underline = true },
+	},
 }
 
 local function config()
-  local plugin = require "no-clown-fiesta"
-  return plugin.load(opts)
+	local plugin = require("no-clown-fiesta")
+
+	local loaded = plugin.load(opts)
+
+	-- Set colors for the active status line
+	vim.api.nvim_set_hl(0, "StatusLine", { fg = "#eaeaea", bg = "#040421", ctermfg = "white", ctermbg = "darkgrey" })
+
+	-- Set colors for inactive status lines (in non-current windows)
+	vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#a0a0a0", bg = "#222222", ctermfg = "lightgrey", ctermbg = "grey" })
+
+	-- You can also link a highlight group to another existing group
+	-- vim.api.nvim_set_hl(0, "StatusLineNC", { link = "Normal" })
+
+	return loaded
 end
 
 return {
-  "aktersnurra/no-clown-fiesta.nvim",
-  priority = 1000,
-  config = config,
-  lazy = false,
+	"aktersnurra/no-clown-fiesta.nvim",
+	priority = 1000,
+	config = config,
+	lazy = false,
 }
